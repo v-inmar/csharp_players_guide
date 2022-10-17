533c264165adb6139da8329444bcb8cba9aa0b67
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-10-17 22:30:26 +0100

    Finished Level 8

---
+++ Fourth_Edition_With_DotNet_6/Level_8/Challenge/C1_The_Defense_Of_Consolas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace C1_The_Defense_Of_Consolas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Defense of Consolas";
+            Console.Write("Target Row? ");
+            string rowStringInput = Console.ReadLine();
+            Console.Write("Target Column? ");
+            string columnStringInput = Console.ReadLine();
+
+            int row = Convert.ToInt32(rowStringInput);
+            int column = Convert.ToInt32(columnStringInput);
+
+            /*
+             * These calculations does not use corrections for negative coordinates
+             * i.e. (-1, 0)
+             */
+
+            int leftSideRow = row;
+            int leftSideColumn = column - 1;
+
+            int topSideRow = row + 1;
+            int topSideColumn = column;
+
+            int rightSideRow = row;
+            int rightSideColumn = column + 1;
+
+            int bottomSideRow = row - 1;
+            int bottomSideColumn = column;
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"Left Side: ({leftSideRow},{leftSideColumn})");
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Top Side: ({topSideRow},{topSideColumn})");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Right Side: ({rightSideRow},{rightSideColumn})");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"Bottom Side: ({bottomSideRow},{bottomSideColumn})");
+
+            Console.Beep();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}

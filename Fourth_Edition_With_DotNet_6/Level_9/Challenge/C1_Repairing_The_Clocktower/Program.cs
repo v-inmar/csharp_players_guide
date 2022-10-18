d8c28383b2467e477a14426735c3589cdce19fd9
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-10-18 19:43:09 +0100

    Finished level 9

---
+++ Fourth_Edition_With_DotNet_6/Level_9/Challenge/C1_Repairing_The_Clocktower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C1_Repairing_The_Clocktower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            string numberString = Console.ReadLine();
+
+            int number = Convert.ToInt32(numberString);
+
+            bool isEven = number % 2 == 0;
+
+            if (isEven) Console.WriteLine("Tick");
+            else Console.WriteLine("Tock");
+        }
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_9/Challenge/C2_Watchtower/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C2_Watchtower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the x value: ");
+            string xString = Console.ReadLine();
+            Console.Write("Enter the y value: ");
+            string yString = Console.ReadLine();
+
+            int xValue = Convert.ToInt32(xString);
+            int yValue = Convert.ToInt32(yString);
+            string message = "The enemy is to the ";
+
+            if (xValue < 0 && yValue > 0) message += "northwest";
+            if (xValue < 0 && yValue == 0) message += "west";
+            if (xValue < 0 && yValue < 0) message += "southwest";
+            if (xValue == 0 && yValue > 0) message += "north";
+            if (xValue == 0 && yValue < 0) message += "south";
+            if (xValue > 0 && yValue > 0) message += "northeast";
+            if (xValue > 0 && yValue == 0) message += "east";
+            if (xValue > 0 && yValue < 0) message += "southeast";
+
+            string toDisplay = xValue == 0 && yValue == 0 ? "The enemy is here!" : message;
+
+            Console.WriteLine(toDisplay);
+        }
+    }
+}

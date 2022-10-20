4f1929504af147d8e9298a2786f5653dc1f33a9c
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-10-20 20:36:48 +0100

    Finished level 11

---
+++ Fourth_Edition_With_DotNet_6/Level_11/Challenge/C1_The_Prototype/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace C1_The_Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber;
+            while(true)
+            {
+                Console.Write("Pilot please enter a number: ");
+                string secretNumberString = Console.ReadLine();
+                secretNumber = Convert.ToInt32(secretNumberString);
+
+                // Make sure secret number is within the range
+                if (secretNumber < 0 || secretNumber > 100)
+                    Console.WriteLine("Only numbers between 0 - 100");
+                else
+                    break;
+            }
+            Console.Clear();
+
+            
+            while(true)
+            {
+                Console.Write("Hunter, what is the Pilot's number: ");
+                string guessedNumberString = Console.ReadLine();
+                int guessedNumber = Convert.ToInt32(guessedNumberString);
+
+                // Make sure guess is in range. Maybe not so ideal but ok for this
+                if(guessedNumber < 0 || guessedNumber > 100)
+                {
+                    Console.WriteLine("Only numbers between 0 - 100");
+                    continue;
+                }
+
+                // Check guess with secret
+                if(guessedNumber < secretNumber)
+                {
+                    Console.WriteLine("Your guess is too low");
+                }else if(guessedNumber > secretNumber)
+                {
+                    Console.WriteLine("Your guess is too high");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Hunter, you correctly guessed the Pilot's number: {secretNumber}");
+
+        }
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_11/Challenge/C2_The_Magic_Cannon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C2_The_Magic_Cannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <=100; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Fire and Electric");
+                    Console.ResetColor();
+                }else if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Fire");
+                    Console.ResetColor();
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Electric");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("Normal");
+                }
+
+
+
+            }
+            
+        }
+    }
+}

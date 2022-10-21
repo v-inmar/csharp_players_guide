2c3b23f2e9051512047591890ae006c1e0c957a9
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-10-21 20:20:11 +0100

    Finished level 12

---
+++ Fourth_Edition_With_DotNet_6/Level_12/Challenge/C1_The_Replicator_of_D_To/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C1_The_Replicator_of_D_To
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userInputArray = new int[5];
+            
+            // User insert array values
+            for(int i = 0; i < userInputArray.Length; i++)
+            {
+                Console.Write($"Enter a number for index {i}: ");
+                userInputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Copy array value into new array
+            int[] copyArray = new int[userInputArray.Length];
+            for(int i = 0; i < userInputArray.Length; i++)
+            {
+                copyArray[i] = userInputArray[i];
+            }
+
+            // Display value of both array one index at a time
+            for(int i = 0; i < userInputArray.Length; i++)
+            {
+                Console.WriteLine($"User Input Array on index: {i} has a value of {userInputArray[i]}");
+                Console.WriteLine($"Copy Array on index: {i} has a value of {copyArray[i]}");
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_12/Challenge/C2_The_Laws_Of_Freach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C2_The_Laws_Of_Freach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4,51,-7,13,-99,15,-8,45,90};
+            int total = 0;
+            foreach(int item in array)
+            {
+                total += item;
+            }
+
+            float average = (float)total / array.Length;
+            Console.WriteLine(average);
+        }
+    }
+}

950f25a98d6d71c56c2db109cb7f115d4b91567f
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-10-22 19:12:24 +0100

    Finished level 13

---
+++ Fourth_Edition_With_DotNet_6/Level_13/Challenge/C1_Taking_A_Number/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace C1_Taking_A_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = AskForNumber("Enter a number");
+            Console.WriteLine(firstNumber);
+
+            int rangeNumber = AskForNumberInRange("Enter a number between 0 and 100", 0, 100);
+            Console.WriteLine(rangeNumber);
+        }
+
+        /// <summary>
+        /// Ask for a number
+        /// </summary>
+        /// <param name="text">Display message for asking user a number</param>
+        /// <returns>A 32 bit integer</returns>
+        static int AskForNumber(string text)
+        {
+            Console.WriteLine(text);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Ask for a number between the range of min and max
+        /// </summary>
+        /// <param name="text">Display message for asking user a number</param>
+        /// <param name="min">The floor of the range</param>
+        /// <param name="max">The ceiling of the range</param>
+        /// <returns>A 32 bit integer</returns>
+        static int AskForNumberInRange(string text, int min, int max)
+        {
+            while (true)
+            {
+                Console.WriteLine(text);
+                int value = Convert.ToInt32(Console.ReadLine());
+                if(value < min)
+                {
+                    Console.WriteLine("Number entered is too low");
+                    continue;
+                }
+
+                if(value > max)
+                {
+                    Console.WriteLine("Number entered is too high");
+                    continue;
+                }
+
+                return value;
+            }
+        }
+    }
+
+
+    // Candidate challenge that can use this is the challenge called The Prototype on Level 11
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_13/Challenge/C2_Countdown/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C2_Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Countdown(10);
+        }
+
+        /// <summary>
+        /// Recursively display the numbers
+        /// </summary>
+        /// <param name="number">The base case where the countdown begins</param>
+        static void Countdown(int number)
+        {
+            if(number == 0) return;
+            Console.WriteLine(number);
+            Countdown(number - 1);
+        }
+    }
+}

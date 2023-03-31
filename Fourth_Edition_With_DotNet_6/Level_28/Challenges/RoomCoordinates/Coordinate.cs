a30d6e6b89062efffcdf31e0daafee517c966a85
Vinmar Tipay <vinmar.tipay@outlook.com>
2023-03-31 23:20:02 +0100

    Finished Level 28 and updated the score

---
+++ Fourth_Edition_With_DotNet_6/Level_28/Challenges/RoomCoordinates/Coordinate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomCoordinates
+{
+    public struct Coordinate
+    {
+        public readonly int Row { get; }
+        public readonly int Column { get; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        /// <summary>
+        /// Check if a coordinate is adjacent to this instance
+        /// </summary>
+        /// <param name="c">Coordinate instance to be checked against</param>
+        /// <returns>Boolean</returns>
+        public bool IsAdjacent(Coordinate c)
+        {
+            if(Math.Abs(Row - c.Row) == 1 && (Math.Abs(Column - c.Column)) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_28/Challenges/RoomCoordinates/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RoomCoordinates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Coordinate[] coordinates = new Coordinate[5];
+            coordinates[0] = new Coordinate(1,2);
+            coordinates[1] = new Coordinate(2, 3);
+            coordinates[2] = new Coordinate(3, 4);
+            coordinates[3] = new Coordinate(4, 5);
+            coordinates[4] = new Coordinate(5, 1);
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                Console.WriteLine($"Coordinate[{i}] has Row: {coordinates[i].Row} and Column: {coordinates[i].Column}");
+            }
+
+            for (int i = 1; i < coordinates.Length; i++)
+            {
+                Console.WriteLine($"Is Coordinate[{i-1}] and Coordinate[{i}] adjacent? {coordinates[i - 1].IsAdjacent(coordinates[i])}");
+            }
+        }
+    }
+}

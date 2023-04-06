efd154fc9815a3dde90bc55b3b910155e69b8485
Vinmar Tipay <vinmar.tipay@outlook.com>
2023-04-06 20:17:31 +0100

    Finished level 29 and updated scoreboard text

---
+++ Fourth_Edition_With_DotNet_6/Level_29/Challenges/WarPreparations/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPreparations
+{
+    public class Enums
+    {
+        public enum MaterialType { Wood, Bronze, Iron, Steel, Binarium };
+        public enum GemstoneType { Emeral, Amber, Sapphire, Diamond, Bitstone, None };
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_29/Challenges/WarPreparations/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WarPreparations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sword original = new(Enums.MaterialType.Iron, Enums.GemstoneType.None, 1, 1);
+
+            Sword firstSword = original with { Material = Enums.MaterialType.Steel, Gemstone = Enums.GemstoneType.Diamond, Length = 10 };
+            Sword secondSword = original with { Material = Enums.MaterialType.Binarium, Gemstone = Enums.GemstoneType.Bitstone, Length = 15, CrossguardWidth = 3 };
+
+            Console.WriteLine(original);
+            Console.WriteLine(firstSword);
+            Console.WriteLine(secondSword);
+        }
+    }
+}

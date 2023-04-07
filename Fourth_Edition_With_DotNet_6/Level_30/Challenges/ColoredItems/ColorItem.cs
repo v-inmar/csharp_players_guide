36b986177ad3eab580af27480212a65547800527
Vinmar Tipay <vinmar.tipay@outlook.com>
2023-04-07 16:10:39 +0100

    Finished level 30 and updated scoreboard text

---
+++ Fourth_Edition_With_DotNet_6/Level_30/Challenges/ColoredItems/ColorItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColoredItems
+{
+    public class ColorItem<T> where T : IItem // Contrainted to objects that implements IItem interface
+    {
+        public ConsoleColor Color { get; set; }
+        public T Item { get; set; }
+
+        public ColorItem(ConsoleColor color, T item)
+        {
+            Color = color;
+            Item = item;
+        }
+
+        public void Display()
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine(Item);
+        }
+    }
+}
---
+++ Fourth_Edition_With_DotNet_6/Level_30/Challenges/ColoredItems/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ColoredItems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorItem<Sword> swordItem = new(ConsoleColor.Red, new Sword());
+            ColorItem<Bow> bowItem = new(ConsoleColor.Blue, new Bow());
+            ColorItem<Axe> axeItem = new(ConsoleColor.Yellow, new Axe());
+
+            swordItem.Display();
+            bowItem.Display();
+            axeItem.Display();
+        }
+    }
+}
